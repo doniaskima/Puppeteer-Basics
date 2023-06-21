1bdcf2ab2554b0c1125ca80bdeb19209f5ee7cac
unknown <doniaskima344@gmail.com>
2023-06-21 19:09:06 +0100

    feat(2.create a pdf from url or html content by running node pdf)

--- pdf.js
+++ pdf.js
@@ -1,20 +1,26 @@
 const puppeteer = require('puppeteer');
 
-(async ()=>{
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    //Create pdf from url 
-    await page.goto('https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pdf');
-    await page.pdf({ path: 'api.pdf', format: 'A4' })
-
-    //create PDF from static HTML
-    const htmlContent = `<body>
-    <h1>An example static HTML to PDF</h1>
-    </body>`
-    await page.setContent(htmlContent);
-    await page.pdf({ path: 'html.pdf', format: 'A4' })
-
-    await browser.close()
-
-})
+try {
+    const puppeteer = require('puppeteer');
+    
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+  
+      // Create pdf from URL
+      await page.goto('https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pdf');
+      await page.pdf({ path: 'api.pdf', format: 'A4' });
+  
+      // Create PDF from static HTML
+      const htmlContent = `<body>
+      <h1>An example static HTML to PDF</h1>
+      </body>`;
+      await page.setContent(htmlContent);
+      await page.pdf({ path: 'html.pdf', format: 'A4' });
+  
+      await browser.close();
+    })();
+  } catch (error) {
+    console.error(error);
+  }
+  
